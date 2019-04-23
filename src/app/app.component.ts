435f4e5e1214060ce8086830eb552f994ef8c8e2
azessai <azeddine.essai@gmail.com>
2019-04-23 23:43:54 +0200

    Version finie avec Lorem Ipsum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,8 @@ type Posts = Array<Post>;
 
 export class AppComponent {
 
-  mesposts: Posts = [ new Post('Mon premier', 'Bla Blad qsd sqd sd qd qd sd qsd qsd dqdsd s. sdqsd '),
-                      new Post('mon deuxième', 'Bla Bla'),
-                      new Post('mon troisième', 'Bla Bla')];
+  mesposts: Posts = [ new Post('Mon premier', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non pretium neque. Nulla congue porta neque id egestas. Integer nec massa fringilla, molestie nunc vitae, varius dolor.'),
+                      new Post('mon deuxième', 'Suspendisse varius, mi et bibendum facilisis, erat massa tempus enim, eget gravida nisi dolor ac tortor. Vivamus vestibulum id ex sit amet vehicula. Mauris vel euismod ipsum.'),
+                      new Post('mon troisième', 'Curabitur massa purus, porta id sodales sit amet, pretium quis odio. In suscipit nunc massa, in sodales nunc mollis nec. Morbi non pellentesque purus.')];
 
 }
