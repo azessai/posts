c298cf5054297385db638c991854e406a016ea64
azessai <azeddine.essai@gmail.com>
2019-12-19 21:08:31 +0100

    sans firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase/app';
-import 'firebase/database';
+
 
 @Component({
   selector: 'app-root',
@@ -8,19 +7,5 @@ import 'firebase/database';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: 'AIzaSyDC0KKDc3IQmb72dcOMbErTOtqyJQVQzPM',
-      authDomain: 'posts-9ce0e.firebaseapp.com',
-      databaseURL: 'https://posts-9ce0e.firebaseio.com',
-      projectId: 'posts-9ce0e',
-      storageBucket: 'posts-9ce0e.appspot.com',
-      messagingSenderId: '620578461465',
-      appId: '1:620578461465:web:7269368098d5b28fa05788',
-      measurementId: 'G-0LGW1WL4B6'
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
+  constructor() {}
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../Models/Post';
 import { Subject } from 'rxjs';
-import * as firebase from 'firebase/app';
-import 'firebase/database';
+
 
 @Injectable(
 {
@@ -19,11 +18,7 @@ export class PostsService {
     this.potsSubject.next(this.posts);
   }
   getPosts() {
-    firebase.database().ref('/posts')
-    .on('value', (data) => {
-      this.posts = data.val() ? data.val() : [];
-      this.emitPosts();
-    });
+    this.emitPosts();
   }
   deletePost(id: number) {
     this.posts.splice(id, 1);
@@ -36,7 +31,6 @@ export class PostsService {
     this.emitPosts();
   }
   savePosts() {
-    firebase.database().ref('/posts').set(this.posts);
   }
   updatePost(id: number, loveIts: number) {
     this.posts[id].loveIts = loveIts;
